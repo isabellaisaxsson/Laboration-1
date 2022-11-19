57f146954d35b363227e43fd89d30cb95eb75463
isabellaisaxsson <isabella.isaksson2@outlook.com>
2022-11-19 21:23:37 +0100

    Update game.js

--- game.js
+++ game.js
@@ -26,6 +26,80 @@ else if(questionPeach == N){
     
     if(questionN = Y){
 
+        alert("Mario said that Bowser ran in the direction of his Evil Tower");
+        var questionMario = prompt("Outside of the Evil Tower: Lulu - the door is locked! How are we supposed to open it? pick up axe or pick up scissor")
+
+        if(questionMario == "pick up axe"){
+
+            alert("Lulu - Hit it against the door!");
+
+            var questionDoor = prompt("Yes! We did it! Now we have to be very quiet, okay? Yes or No");
+
+            if(questionDoor == Y){
+                alert("Lulu - Thank you" + questionName + "! Now let's go upstairs");
+
+                var questionWeapon = prompt("Outside of Bowsers room: Lulu - So I have my powers but which weapon do you want? sword or arrows");
+
+                if(questionWeapon == "sword"){
+
+                    var questionFight = prompt("Inside of Bowsers room: stab Bowser or hit Bowser with the sword");
+
+                    if(questionFight == "stab Bowser"){
+
+                        alert("Lulu - Bowser is down! Here is Marios hat");
+
+                        alert("Lulu - Thank you for the help" + questionName + "! Goodbye, hope I see you soon!");
+
+                    }
+
+                    else if(questionFight == "hit Bowser with the sword"){
+
+                        alert("Lulu - Bowser is almost down! Here take the hat!")
+
+                        alert("Lulu - Thank you for the help" + questionName + "! Goodbye, hope I see you soon!");
+                    }
+
+                }
+
+                else if(questionWeapon == "arrow"){
+
+                    var questionFightArrow = prompt("Inside of Bowsers room: hit Bowser with arrow or stab Bowser with arrow");
+
+                    if(questionFight == "hit Bowser"){
+
+                        alert("Lulu - Bowser is down! Here is Marios hat");
+
+                        alert("Lulu - Thank you for the help" + questionName + "! Goodbye, hope I see you soon!");
+
+                    }
+
+                    else if(questionFight == "stab Bowser"){
+
+                        alert("Lulu - Bowser is almost down! Here take the hat!")
+
+                        alert("Lulu - Thank you for the help" + questionName + "! Goodbye, hope I see you soon!");
+                    }
+            }
+
+        }
+
+      else if(questionDoor == N){
+
+            alert("Oh no! Bowser has caught us! Now we will never get back Marios hat!");
+
+            alert("GAME OVER")
+
+        }
+    }
+
+    else if(questionMario == "pick up scissor"){
+
+        alert("Lulu - Try to hit it against the door!");
+
+        alert("Lulu - Well that didn't word");
+
+        alert("GAME OVER");
+
     }
 
     else(questionN = N){
@@ -34,6 +108,7 @@ else if(questionPeach == N){
     }
 }
 
-else{
+else {
     alert("Please answer Yes or No!")
+
 }
